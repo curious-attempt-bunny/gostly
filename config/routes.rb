8ea66ab31db9bddac64a0e9a1946be88343f9dd1
Merlyn Albery-Speyer <merlyn@newrelic.com>
2013-12-05 07:59:07 -0800

    Fix routing for EmailsController

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Gostly::Application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'home#index'
 
-  post 'mailgun/email/receive' => 'email#post'
+  post 'mailgun/email/receive' => 'emails#post'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
