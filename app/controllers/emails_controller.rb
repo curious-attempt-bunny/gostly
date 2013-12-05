6a17e185bd385eb78bb57254cc4fcb312e33da37
Merlyn Albery-Speyer <merlyn@newrelic.com>
2013-12-04 20:38:24 -0800

    Add EmailsController from example Mailgun documentation

---
+++ app/controllers/emails_controller.rb
@@ -0,0 +1,27 @@
+# Based on https://devcenter.heroku.com/articles/mailgun#receiving-messages-via-http
+
+class EmailsController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+
+  def post
+    # process various message parameters:
+    sender  = params['from']
+    subject = params['subject']
+
+    # get the "stripped" body of the message, i.e. without
+    # the quoted part
+    actual_body = params["stripped-text"]
+
+    # process all attachments:
+    count = params['attachment-count'].to_i
+    Rails.logger.error "Email received From #{sender}, Subject #{subject}, Body: #{body}, Attachments: #{count}"
+
+    count.times do |i|
+      stream = params["attachment-#{i+1}"]
+      filename = stream.original_filename
+      data = stream.read()
+      Rails.logger.error "  Attachment #{i} Filename: #{filename}"
+    end
+    render :text => "OK"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Gostly::Application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'home#index'
 
+  post 'mailgun/email/receive' => 'email#post'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
