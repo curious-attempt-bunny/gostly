671957c4b3bfe249276357b66fb0d5bae605a713
Merlyn Albery-Speyer <merlyn@newrelic.com>
2014-01-01 12:24:23 -0800

    Switch to the actual mailgun behaviour

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,29 +1,20 @@
 require 'images/upload'
 
-# Based on https://devcenter.heroku.com/articles/mailgun#receiving-messages-via-http
-
 class EmailsController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def post
-    # process various message parameters:
-    sender  = params['from']
-    subject = params['subject']
+    sender      = params['sender']
+    subject     = params['Subject']
+    body        = params["stripped-text"]
+    attachments = params["attachments"]
 
-    # get the "stripped" body of the message, i.e. without
-    # the quoted part
-    actual_body = params["stripped-text"]
+    Rails.logger.error "Email received From #{sender}, Subject #{subject}, Body: #{body}, Attachments: #{attachments.size}"
 
-    # process all attachments:
-    count = params['attachment-count'].to_i
-    Rails.logger.error "Email received From #{sender}, Subject #{subject}, Body: #{actual_body}, Attachments: #{count}"
-
-    count.times do |i|
-      stream = params["attachment-#{i+1}"]
-      filename = stream.original_filename
-      data = stream.read()
-      Rails.logger.error "  Attachment #{i} Filename: #{filename}"
-      url = Images::Upload.new.upload(data)
+    attachments.each_with_index do |attachment, index|
+      Rails.logger.error "  Attachment #{index} Filename: #{attachment["name"]} URL: #{attachment["url"]}"
+      
+      url = Images::Upload.new.upload(attachment["url"])
       Image.create!(url: url, email_from: sender, email_subject: subject) if url
     end
     render :text => "OK"
